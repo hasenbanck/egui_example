5b7e9075b804cd8ede233ac4cae60fa767fbc453
Nils Hasenbanck <nils@hasenbanck.de>
2020-10-11 17:14:12 +0200

    Simplify example.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use egui::demos::DemoWindow;
+use std::iter;
+use std::time::Instant;
+
 use egui::paint::FontDefinitions;
 use egui_winit::WinitPlatformDescriptor;
 use futures_lite::future::block_on;
-use std::iter;
-use std::sync::Arc;
-use std::time::Instant;
 use winit::event::Event::*;
 use winit::event_loop::ControlFlow;
 
@@ -55,22 +54,22 @@ fn main() {
         format: OUTPUT_FORMAT,
         width: size.width as u32,
         height: size.height as u32,
-        present_mode: wgpu::PresentMode::Fifo,
+        present_mode: wgpu::PresentMode::Mailbox,
     };
     let mut swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
     // We use the egui_winit crate as the platform.
     let mut platform = egui_winit::WinitPlatform::new(WinitPlatformDescriptor {
         scale_factor: window.scale_factor(),
-        font_definitions: FontDefinitions::with_pixels_per_point(14.0),
+        font_definitions: FontDefinitions::with_pixels_per_point(window.scale_factor() as f32),
         style: Default::default(),
     });
 
     // We use the egui_wgpu crate as the render backend.
     let mut egui_rpass = egui_wgpu::EguiRenderPass::new(&device, OUTPUT_FORMAT);
 
-    // Use a simple demo UI.
-    let mut app = DemoApp::default();
+    // Display simple demo window.
+    let mut demo_window = egui::demos::DemoWindow::default();
 
     let start_time = Instant::now();
     event_loop.run(move |event, _, control_flow| {
@@ -88,12 +87,15 @@ fn main() {
                     }
                 };
 
-                let mut ui = platform.begin_frame();
+                // Begin to draw the UI frame.
+                let ui = platform.begin_frame();
 
                 // Draw the egui based UI.
-                app.ui(&mut ui);
+                egui::Window::new("Demo").scroll(true).show(ui.ctx(), |ui| {
+                    demo_window.ui(ui);
+                });
 
-                // The outputs could be inspected now and handled.
+                // End the UI frame. We could now handle the output and draw the UI with the backend.
                 let (_output, paint_jobs) = platform.end_frame();
 
                 let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
@@ -121,7 +123,6 @@ fn main() {
             }
             MainEventsCleared => {
                 window.request_redraw();
-                *control_flow = ControlFlow::Wait;
             }
             WindowEvent { event, .. } => match event {
                 winit::event::WindowEvent::Resized(size) => {
@@ -137,119 +138,4 @@ fn main() {
             _ => (),
         }
     });
-}
-
-/// A simple demonstration how to draw with egui.
-pub struct DemoApp {
-    demo_window: egui::demos::DemoWindow,
-}
-
-impl DemoApp {
-    pub fn ui(&mut self, mut ui: &mut egui::Ui) {
-        self.show_menu_bar(&mut ui);
-        self.windows(ui.ctx());
-    }
-
-    /// Show the open windows.
-    fn windows(&mut self, ctx: &Arc<egui::Context>) {
-        egui::Window::new("Demo").scroll(true).show(ctx, |ui| {
-            self.demo_window.ui(ui);
-        });
-
-        egui::Window::new("Settings").show(ctx, |ui| {
-            ctx.settings_ui(ui);
-        });
-
-        egui::Window::new("Inspection")
-            .scroll(true)
-            .show(ctx, |ui| {
-                ctx.inspection_ui(ui);
-            });
-
-        egui::Window::new("Memory")
-            .resizable(false)
-            .show(ctx, |ui| {
-                ctx.memory_ui(ui);
-            });
-
-        self.resize_windows(ctx);
-    }
-
-    fn show_menu_bar(&self, ui: &mut egui::Ui) {
-        egui::menu::bar(ui, |ui| {
-            egui::menu::menu(ui, "File", |ui| {
-                if ui.button("Reorganize windows").clicked {
-                    ui.ctx().memory().reset_areas();
-                }
-                if ui
-                    .button("Clear entire Egui memory")
-                    .on_hover_text("Forget scroll, collapsibles etc")
-                    .clicked
-                {
-                    *ui.ctx().memory() = Default::default();
-                }
-            });
-            egui::menu::menu(ui, "About", |ui| {
-                ui.label("This is Egui");
-                ui.add(
-                    egui::Hyperlink::new("https://github.com/emilk/egui").text("Egui home page"),
-                );
-            });
-        });
-    }
-
-    fn resize_windows(&mut self, ctx: &Arc<egui::Context>) {
-        egui::Window::new("resizable")
-            .scroll(false)
-            .resizable(true)
-            .show(ctx, |ui| {
-                ui.label("scroll:    NO");
-                ui.label("resizable: YES");
-                ui.label(egui::demos::LOREM_IPSUM);
-            });
-
-        egui::Window::new("resizable + embedded scroll")
-            .scroll(false)
-            .resizable(true)
-            .default_height(300.0)
-            .show(ctx, |ui| {
-                ui.label("scroll:    NO");
-                ui.label("resizable: YES");
-                ui.heading("We have a sub-region with scroll bar:");
-                egui::ScrollArea::auto_sized().show(ui, |ui| {
-                    ui.label(egui::demos::LOREM_IPSUM_LONG);
-                    ui.label(egui::demos::LOREM_IPSUM_LONG);
-                });
-                // ui.heading("Some additional text here, that should also be visible"); // this works, but messes with the resizing a bit
-            });
-
-        egui::Window::new("resizable + scroll")
-            .scroll(true)
-            .resizable(true)
-            .default_height(300.0)
-            .show(ctx, |ui| {
-                ui.label("scroll:    YES");
-                ui.label("resizable: YES");
-                ui.label(egui::demos::LOREM_IPSUM_LONG);
-            });
-
-        egui::Window::new("auto_sized")
-            .auto_sized()
-            .show(ctx, |ui| {
-                ui.label("This window will auto-size based on its contents.");
-                ui.heading("Resize this area:");
-                egui::Resize::default().show(ui, |ui| {
-                    ui.label(egui::demos::LOREM_IPSUM);
-                });
-                ui.heading("Resize the above area!");
-            });
-    }
-}
-
-impl Default for DemoApp {
-    fn default() -> Self {
-        Self {
-            demo_window: DemoWindow::default(),
-        }
-    }
 }
